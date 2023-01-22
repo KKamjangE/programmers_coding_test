93d897a90c8fe29a4885e96f64ac16ab593e2627
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-01-22 15:13:39 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,7 +1,3 @@
 function solution(money) {
-    var answer = [];
-    let AA = Math.floor(money/5500);
-    answer.push(AA);
-    answer.push(money-AA*5500);
-    return answer;
+    return [Math.floor(money/5500), Math.floor(money%5500)]
 }
