e43e9b91f5eeb04ffea7062f73caf5ff854bbbcc
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-03-17 17:51:20 +0900

    [Silver III] Title: N과 M (2), Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+visited = [False] * (N + 1)
+
+s = []
+
+def dfs(start):
+    if len(s) == M:
+        print(" ".join(map(str, s)))
+        return
+    for i in range(start, N + 1):
+        if visited[i]:
+            continue
+        visited[i] = True
+        s.append(i)
+        dfs(i)
+        s.pop()
+        visited[i] = False
+        
+dfs(1)
