1a75bcec434b86e616622aaaa83d0aa58635116c
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-03-17 15:55:15 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,11 @@
+import sys
+import itertools
+input = sys.stdin.readline
+
+arr = [int(input()) for _ in range(9)]
+
+for i in itertools.combinations(arr, 7):
+    if sum(i) == 100:
+        a = sorted(i)
+        print(*a, sep='\n')
+        break
