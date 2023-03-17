bb9b82fa93a77b1efdebcb19ff2d6a956cea1ee0
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-03-17 15:10:00 +0900

    [Bronze V] Title: 팩토리얼, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+def factorial(n):
+    if n > 0:
+        return n * factorial(n - 1)
+    else:
+        return 1
+    
+print(factorial(N))
