6bd3a7571108e3e4f16e30a746031d586b0e879b
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-03-20 20:28:09 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -0,0 +1,12 @@
+function solution(x) {
+  x += "";
+  sum = 0;
+  for (let i = 0; i < x.length; i++) {
+    sum += Number(x[i]);
+  }
+  if (x % sum == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
