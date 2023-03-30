59878b2a748fcde1455a69234126a859cfb9dce4
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-03-31 00:26:28 +0900

    [level 3] Title: 가장 먼 노드, Time: 21.80 ms, Memory: 20 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -2,22 +2,20 @@ from collections import deque
 
 def solution(n, edge):
     graph = [[] for _ in range(n + 1)]
-    visit = [0] * (n + 1)
-    visit[1] = 1
-    for e in edge:
-        a, b = e
+    visited = [0] * (n + 1)
+    for a, b in edge:
         graph[a].append(b)
         graph[b].append(a)
 
-    def dijkstra(start):
-        que = deque([start])
+    visited[1] = 1
+    que = deque([1])
 
-        while que:
-            now = que.popleft()
-            for i in graph[now]:
-                if not visit[i]:
-                    visit[i] = visit[now] + 1
-                    que.append(i)
-
-    dijkstra(1)
-    return visit.count(max(visit))
+    while que:
+        now = que.popleft()
+        
+        for i in graph[now]:
+            if not visited[i]:
+                visited[i] = visited[now] + 1
+                que.append(i)
+                
+    return visited.count(max(visited))
