59b2cfcfd8d731df4308c71f7dd15991800071be
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-04-01 20:49:33 +0900

    [level 2] Title: 피로도, Time: 0.09 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -1,7 +1,7 @@
 def solution(k, dungeons):
     ans = 0
     visited = [False] * len(dungeons)
-
+    
     def dfs(k, count):
         nonlocal ans
         ans = max(ans, count)
@@ -9,7 +9,8 @@ def solution(k, dungeons):
         for i in range(len(dungeons)):
             if k >= dungeons[i][0] and not visited[i]:
                 visited[i] = True
-                dfs(k - dungeons[i][1] ,count + 1)
+                dfs(k - dungeons[i][1], count + 1)
                 visited[i] = False
+        
     dfs(k, 0)
     return ans
