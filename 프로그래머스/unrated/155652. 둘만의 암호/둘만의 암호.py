f2e4a76c8dcbea6e23be09fb8aa03e3a9830878c
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-04-11 18:19:51 +0900

    [unrated] Title: 둘만의 암호, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -0,0 +1,11 @@
+def solution(s, skip, index):
+    ans = ''
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    
+    for i in skip:
+        alpha = alpha.replace(i, "")
+    
+    for i in s:
+        ans += alpha[(alpha.index(i) + index) % len(alpha)]
+    
+    return ans
