158011dff563828dee6db4597a2ddd3ce1dc394e
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-04-12 20:54:19 +0900

    [level 3] Title: 네트워크, Time: 0.14 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,15 +1,23 @@
+from collections import deque
+
 def solution(n, computers):
     ans = 0
-    def dfs(i):
-        visited[i] = True
-        for j in range(n):
-            if computers[i][j] and not visited[j]:
-                dfs(j)
-
     visited = [False] * n
+
+    def bfs(start):
+        que = deque([start])
+        visited[start] = True
+        
+        while que:
+            q = que.popleft()
+            for i in range(n):
+                if not visited[i] and computers[q][i]:
+                    visited[i] = True
+                    que.append(i)
+
     for i in range(n):
         if not visited[i]:
-            dfs(i)
+            bfs(i)
             ans += 1
 
     return ans
