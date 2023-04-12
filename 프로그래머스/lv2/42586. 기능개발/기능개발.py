4087f9e293975e5910810c99f01059e83c121fcd
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-04-12 16:11:00 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,21 +1,19 @@
 from collections import deque
 
 def solution(progresses, speeds):
-    answer = []
-    count = 1
+    ans = []
+    days = 1
     tmp = 0
-    que = deque()
-    for i in range(len(progresses)):
-        que.append((progresses[i], speeds[i]))
 
-    while que:
-        if que[0][0] + (count * que[0][1]) >= 100:
-            que.popleft()
+    while progresses:
+        if progresses[0] + (speeds[0] * days) >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
             tmp += 1
         else:
-            answer.append(tmp)
-            count += 1
+            ans.append(tmp)
             tmp = 0
-    answer.append(tmp)
-
-    return [p for p in answer if 0 < p]
+            days += 1
+    ans.append(tmp)
+    
+    return [q for q in ans if q > 0]
