8c42fbe2fa5743ffb671f628946d43298b7aad5d
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-04-13 17:05:24 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -1,26 +1,29 @@
 from collections import deque
 
 def solution(begin, target, words):
-    ans = 0
     visited = [False] * len(words)
-    que = deque()
-    que.append([begin, 0])
+    
+    def bfs(start, depth):
+        que = deque()
+        que.append([start, depth])
         
-    while que:
-        q, depth = que.popleft()
-
-        if q == target:
-            ans = depth
-            break
-
-        for i in range(len(words)):
-            count = 0
-            if not visited[i]:
-                for j in range(len(words[i])):
-                    if q[j] != words[i][j]:
-                        count += 1
-                if count == 1:
-                    que.append([words[i], depth + 1])
-                    visited[i] = True
-
-    return ans
+        while que:
+            q, dep = que.popleft()
+            
+            if q == target:
+                return dep
+            
+            for i in range(len(words)):
+                count = 0
+                if not visited[i]:
+                    for j in range(len(words[i])):
+                        if q[j] != words[i][j]:
+                            count += 1
+                        if count > 1:
+                            break
+                    if count == 1:
+                        que.append([words[i], dep + 1])
+                        visited[i] = True
+        return 0
+    
+    return bfs(begin, 0)
