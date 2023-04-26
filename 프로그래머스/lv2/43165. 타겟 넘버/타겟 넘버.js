5b45f529810408a2228e4b251ab9bbe009519105
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-04-26 14:23:38 +0900

    [level 2] Title: 타겟 넘버, Time: 14.09 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,19 +1,71 @@
+class Node {
+  constructor(item) {
+    this.item = item;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  enqueue(item) {
+    const node = new Node(item);
+    if (!this.length) {
+      this.tail = node;
+      this.head = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+  }
+
+  dequeue() {
+    let pop = null;
+    if (!this.length) return null;
+    else if (this.head === this.tail) {
+      pop = this.head.item;
+      this.head = null;
+      this.tail = null;
+    } else {
+      pop = this.head.item;
+      this.head = this.head.next;
+    }
+    this.length--;
+    return pop;
+  }
+
+  size() {
+    return this.length;
+  }
+}
+
 function solution(numbers, target) {
   var ans = 0;
 
-  const DFS = (sum, depth) => {
-    if (depth === numbers.length) {
-      if (sum === target) {
+  const BFS = (depth) => {
+    let que = new Queue();
+    que.enqueue([-numbers[depth], depth + 1]);
+    que.enqueue([numbers[depth], depth + 1]);
+
+    while (que.size() > 0) {
+      let [sum, dep] = que.dequeue();
+      if (sum === target && dep === numbers.length) {
         ans += 1;
       }
-      return;
-    }
 
-    DFS(sum - numbers[depth], depth + 1);
-    DFS(sum + numbers[depth], depth + 1);
+      if (dep < numbers.length) {
+        que.enqueue([sum + numbers[dep], dep + 1]);
+        que.enqueue([sum - numbers[dep], dep + 1]);
+      }
+    }
   };
 
-  DFS(0, 0);
+  BFS(0);
 
   return ans;
 }
