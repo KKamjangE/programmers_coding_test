8b72613ed2bfcdf577df473bc9ecc85f1710c9c3
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-05-01 16:24:37 +0900

    [unrated] Title: 카운트 업, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.js
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.js
@@ -1,7 +1,3 @@
 function solution(start, end) {
-  let ans = [];
-  for (let i = start; i <= end; i++) {
-    ans.push(i);
-  }
-  return ans;
+  return Array.from({ length: end - start + 1 }, () => start++);
 }
