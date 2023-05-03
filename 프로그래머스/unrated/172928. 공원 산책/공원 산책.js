2855c19b292d47f2f45b1b6c6dfb98457b002dbc
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-05-03 15:08:35 +0900

    [unrated] Title: 공원 산책, Time: 0.35 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.js
@@ -1,7 +1,8 @@
 function solution(park, routes) {
-  let newPark = park.map((value) => value.split(""));
+  let newPark = park.map((value) => value.split("")); // park를 2차원 배열로
   let x, y;
 
+  // 방향 컨트롤러
   let controls = {
     N: [-1, 0],
     S: [1, 0],
@@ -9,6 +10,7 @@ function solution(park, routes) {
     E: [0, 1],
   };
 
+  // 시작 위치 초기화
   newPark.forEach((value, idxY) => {
     value.forEach((val, idxX) => {
       if (val === "S") {
@@ -19,10 +21,10 @@ function solution(park, routes) {
   });
 
   routes.forEach((value) => {
-    let [control, whight] = value.split(" ");
-    let check = true;
+    let [control, whight] = value.split(" "); // 방향, 거리
     let newX = x,
       newY = y;
+    // 거리 만큼 반복
     for (let i = 0; i < Number(whight); i++) {
       newY = newY + controls[control][0];
       newX = newX + controls[control][1];
@@ -33,13 +35,11 @@ function solution(park, routes) {
         newX >= newPark[0].length ||
         newPark[newY][newX] === "X"
       ) {
-        check = false;
+        return; // 다음 반복문으로(forEach continue)
       }
     }
-    if (check) {
-      x = newX;
-      y = newY;
-    }
+    x = newX;
+    y = newY;
   });
 
   return [y, x];
