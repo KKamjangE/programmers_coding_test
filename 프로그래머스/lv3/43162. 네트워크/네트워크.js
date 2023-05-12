3dab2510fa558352d5479430b194e0653c4edf9b
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-05-12 18:02:04 +0900

    [level 3] Title: 네트워크, Time: 0.53 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.js
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.js
@@ -1,28 +1,28 @@
 function solution(n, computers) {
   let ans = 0;
-  let visited = Array.from({ length: n }, (n) => false);
+  let visited = Array.from({ length: n }, () => false);
 
-  const bfs = (start) => {
-    que = [start];
-    visited[start] = true;
+  const BFS = (value) => {
+    let que = [value];
 
     while (que.length > 0) {
       q = que.shift();
-      for (let i = 0; i < n; i++) {
-        if (computers[q][i] === 1 && visited[i] === false) {
-          que.push(i);
-          visited[q] = true;
+
+      q.forEach((value, idx) => {
+        if (value === 1 && !visited[idx]) {
+          visited[idx] = true;
+          que.push(computers[idx]);
         }
-      }
+      });
     }
   };
 
-  for (let i = 0; i < n; i++) {
-    if (visited[i] === false) {
-      bfs(i);
-      ans++;
+  computers.forEach((value, idx) => {
+    if (!visited[idx]) {
+      ans += 1;
+      BFS(value);
     }
-  }
+  });
 
   return ans;
 }
