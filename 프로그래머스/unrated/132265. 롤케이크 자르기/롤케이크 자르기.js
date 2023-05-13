c1ee1d6d05b3efcc1b6fbd8f0bde990919bf4e90
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-05-13 20:56:30 +0900

    [unrated] Title: 롤케이크 자르기, Time: 33.03 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -0,0 +1,26 @@
+function solution(topping) {
+  let ans = 0;
+  let right = 0;
+  let left = 0;
+  let [toppingLeft, toppingRight] = [
+    Array(10001).fill(0),
+    Array(10001).fill(0),
+  ];
+  topping.forEach((value) => {
+    if (toppingRight[value] === 0) right += 1;
+    toppingRight[value] += 1;
+  });
+
+  topping.forEach((value) => {
+    if (toppingLeft[value] === 0) {
+      toppingLeft[value] += 1;
+      left += 1;
+    }
+    toppingRight[value] -= 1;
+    if (toppingRight[value] === 0) right -= 1;
+    if (right === left) {
+      ans += 1;
+    }
+  });
+  return ans;
+}
