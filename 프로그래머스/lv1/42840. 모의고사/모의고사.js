f5fcf0b928185df14a214c29bc2595ffbaef2800
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-05-15 22:00:12 +0900

    [level 1] Title: 모의고사, Time: 3.31 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,24 +1,21 @@
 function solution(answers) {
   let ans = [];
-  let people = [
-    [1, 2, 3, 4, 5],
-    [2, 1, 2, 3, 2, 4, 2, 5],
-    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
-  ];
-  let score = [0, 0, 0];
+  let a1 = [1, 2, 3, 4, 5];
+  let a2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  let a3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
-  answers.forEach((value, idx) => {
-    people.forEach((answer, i) => {
-      let tmp = idx % answer.length;
-      if (value === answer[tmp]) {
-        score[i] += 1;
-      }
-    });
-  });
+  let a1c = answers.filter(
+    (value, idx) => value === a1[idx % a1.length]
+  ).length;
+  let a2c = answers.filter(
+    (value, idx) => value === a2[idx % a2.length]
+  ).length;
+  let a3c = answers.filter(
+    (value, idx) => value === a3[idx % a3.length]
+  ).length;
+  let max = Math.max(a1c, a2c, a3c);
 
-  let max = Math.max(...score);
-
-  score.forEach((value, idx) => {
+  [a1c, a2c, a3c].forEach((value, idx) => {
     if (value === max) {
       ans.push(idx + 1);
     }
