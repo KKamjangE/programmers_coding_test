51db7dd3a9be6b2f8cf0f6983a31b63ef8201e2c
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-05-17 00:24:48 +0900

    [unrated] Title: 0 떼기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
@@ -1,14 +1,3 @@
 function solution(n_str) {
-  let ans = "";
-  let check = false;
-  for (let i = 0; i < n_str.length; i++) {
-    if (n_str[i] != "0") {
-      check = true;
-    }
-    if (check) {
-      ans += n_str[i];
-    }
-  }
-
-  return ans;
+  return Number(n_str) + "";
 }
