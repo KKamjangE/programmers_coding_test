d2e15ed0b78d063aa883820851012c0f1704772e
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-05-21 17:05:26 +0900

    [level 2] Title: 택배상자, Time: 16.50 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.js
@@ -0,0 +1,14 @@
+function solution(order) {
+  let ans = 0;
+  let stack = [];
+
+  for (let i = 1; i <= order.length; i++) {
+    stack.push(i);
+    while (stack.length > 0 && stack[stack.length - 1] === order[ans]) {
+      stack.pop();
+      ans += 1;
+    }
+  }
+
+  return ans;
+}
