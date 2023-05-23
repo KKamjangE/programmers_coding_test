89f3a242a0ca19759e190a5d36ad9fbc14867564
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-05-23 19:46:44 +0900

    [level 2] Title: 짝지어 제거하기, Time: 47.47 ms, Memory: 50.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,7 +1,10 @@
 function solution(s) {
-  let stack = [];
+  let stack = []; // 스택 자료구조
 
+  if (s.length % 2 != 0) return 0;
+  // 매개변수 문자열 순회
   [...s].forEach((value) => {
+    // 스택이 비어있지 않고 스택 맨 위 값이 현재 값과 같다면
     if (stack.length && stack[stack.length - 1] === value) {
       stack.pop();
     } else {
