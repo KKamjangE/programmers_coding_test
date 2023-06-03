d6e455901f2f346aad16855c83bcdccc6543098d
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-06-03 19:03:10 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -0,0 +1,33 @@
+function solution(s) {
+  let ans = 0;
+
+  const check = (str) => {
+    const stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === "[") {
+        stack.push(str[i]);
+      } else if (str[i] === "(") {
+        stack.push(str[i]);
+      } else if (str[i] === "{") {
+        stack.push(str[i]);
+      } else if (str[i] === ")" && stack[stack.length - 1] === "(") {
+        stack.pop();
+      } else if (str[i] === "]" && stack[stack.length - 1] === "[") {
+        stack.pop();
+      } else if (str[i] === "}" && stack[stack.length - 1] === "{") {
+        stack.pop();
+      } else {
+        return 0;
+      }
+    }
+    return stack.length > 0 ? 0 : 1;
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    ans += check(s);
+    s = s.substr(1) + s.substr(0, 1);
+  }
+
+  return ans;
+}
