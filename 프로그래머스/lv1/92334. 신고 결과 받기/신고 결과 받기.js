a6c973462aa39e43b2881ce3599c6174422ae741
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-06-09 14:59:11 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,23 +1,28 @@
 function solution(id_list, report, k) {
-  let ans = {};
-  let obj = {};
-  report = [...new Set(report)];
+  let ans = {}; // 처리 결과 메일을 받은 횟수
+  let reportObj = {}; // 신고 받은 횟수
+
+  report = [...new Set(report)]; // 중복된 신고 제거
+
   id_list.forEach((value) => {
-    obj[value] = 0;
+    // 객체 초기화
+    reportObj[value] = 0;
     ans[value] = 0;
   });
 
   report.forEach((value) => {
-    let [_, target] = value.split(" ");
-    obj[target] += 1;
+    // 신고 받은 횟수 카운팅
+    const [_, target] = value.split(" ");
+    reportObj[target] += 1;
   });
 
   report.forEach((value) => {
     let [reporter, target] = value.split(" ");
-    if (obj[target] >= k) {
+    if (reportObj[target] >= k) {
+      // 신고자가 신고한 타겟이 이용 정지라면 받은 이메일 횟수 추가
       ans[reporter] += 1;
     }
   });
 
-  return Object.values(ans).map((value) => value);
+  return Object.values(ans); // 받은 메일 횟수만 배열로 반환
 }
