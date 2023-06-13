6d2c16b9456f1b3c2270ae2297c4b301aeb64a8c
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-06-13 14:08:17 +0900

    [level 2] Title: 소수 찾기, Time: 0.38 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
@@ -1,5 +1,7 @@
 function solution(numbers) {
-  let ans = new Set();
+  let ans = new Set(); // 중복 제거용 Set 자료구조
+  
+  // 소수 판별 함수
   const check = (num) => {
     if (num < 2) return false;
     for (let i = 2; i <= Math.sqrt(num); i++) {
@@ -8,20 +10,20 @@ function solution(numbers) {
     return true;
   };
 
-  const DFS = (numArr, fixedNum) => {
-    if (numArr.length) {
+  const DFS = (numArr, currNum) => {
+    if (numArr.length) { // 배열의 요소가 1개라도 있다면
       for (let i = 0; i < numArr.length; i++) {
-        if (check(parseInt(fixedNum + numArr[i]))) {
-          ans.add(parseInt(fixedNum + numArr[i]));
+        if (check(parseInt(currNum + numArr[i]))) { // 소수 판별
+          ans.add(parseInt(currNum + numArr[i])); // 소수면 추가
         }
-        const tmp = [...numArr];
-        tmp.splice(i, 1);
-        DFS(tmp, fixedNum + numArr[i]);
+        const newNumArr = [...numArr]; // 배열 복사
+        newNumArr.splice(i, 1); // 맨 앞의 배열을 자른다
+        DFS(newNumArr, currNum + numArr[i]); // 재귀
       }
     }
   };
 
-  DFS(numbers.split(""), "");
+  DFS(numbers.split(""), ""); // numbers배열과 빈 문자열을 인자로 넘김
 
-  return ans.size;
+  return ans.size; // 중복이 모두 제거됐기 때문에 길이로 넘겨준다
 }
