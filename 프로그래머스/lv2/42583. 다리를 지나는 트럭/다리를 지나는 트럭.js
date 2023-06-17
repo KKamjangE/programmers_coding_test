7686c30a4e5e5ffd804aa43942738772bf62434a
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-06-17 23:14:21 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 4.67 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,21 +1,19 @@
 function solution(bridge_length, weight, truck_weights) {
-  let time = 1;
-
-  let bridge = new Array(bridge_length).fill(0); // 다리길이만큼 배열 생성
-  let bridge_weight = truck_weights[0]; // 다리가 버티고 있는 무게, 0번째 트럭무게 추가
-  bridge[bridge_length - 1] = truck_weights.shift(); // 0번째 트럭 다리에 진입
-
-  while (bridge_weight > 0) {
-    bridge_weight -= bridge.shift(); // 다리에서 지나간 트럭 무게 빼기(트럭 없으면 0)
-    if (bridge_weight + truck_weights[0] <= weight) {
-      // 현재 다리의 무게 + 다음 트럭 무게가 버틸 수 있는 무게 이하일 경우
-      bridge_weight += truck_weights[0]; // 트럭 무게 더하기
-      bridge.push(truck_weights.shift()); // 다리에 트럭 진입
-    } else {
-      bridge.push(0); // 트럭 대신 0 추가
+    let ans = 1;
+    const arr = new Array(bridge_length).fill(0);
+    let w = truck_weights[0];
+    arr[arr.length - 1] = truck_weights.shift();
+    
+    while(w > 0){
+        w -= arr.shift();
+        if(w + truck_weights[0] <= weight){
+            w += truck_weights[0];
+            arr.push(truck_weights.shift());
+        }else{
+            arr.push(0);
+        }
+        ans += 1;
     }
-    time += 1; // 시간 ++
-  }
-
-  return time;
+    console.log(arr);
+    return ans;
 }
