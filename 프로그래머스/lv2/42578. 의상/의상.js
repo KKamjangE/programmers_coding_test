8e99761700a7224d96de00773313f0dfd9870c8f
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-06-17 21:24:38 +0900

    [level 2] Title: 의상, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.js
+++ 프로그래머스/lv2/42578. 의상/의상.js
@@ -1,15 +1,15 @@
 function solution(clothes) {
-  let ans = 1;
-  let obj = {};
-
-  clothes.forEach(([name, category]) => {
-    if (!obj[category]) obj[category] = 0;
-    obj[category] += 1;
-  });
-
-  Object.keys(obj).forEach((key) => {
-    ans *= obj[key] + 1;
-  });
-
-  return ans - 1;
+    let ans = 1;
+    const obj = {};
+    
+    clothes.forEach(([_, category]) => {
+        if(obj[category]) obj[category] += 1;
+        else obj[category] = 1;
+    })
+    
+    Object.keys(obj).forEach((key) => {
+        ans *= obj[key] + 1;
+    })
+    
+    return ans - 1;
 }
