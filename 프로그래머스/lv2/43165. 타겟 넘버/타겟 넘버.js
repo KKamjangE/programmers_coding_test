cf2ff664d407b3ac8287938fc7930b3ecb812d85
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-06-18 17:46:48 +0900

    [level 2] Title: 타겟 넘버, Time: 1.09 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,19 +1,19 @@
 function solution(numbers, target) {
-  let ans = 0;
-
-  const DFS = (idx, sum) => {
-    if (idx === numbers.length) {
-      if (sum === target) {
-        ans += 1;
-      }
-      return;
+    let ans = 0;
+    
+    const DFS = (currNum, depth) => {
+        if(depth === numbers.length){
+            if(currNum === target){
+                ans += 1;
+            }
+            return;
+        }
+        
+        DFS(currNum + numbers[depth], depth + 1);
+        DFS(currNum - numbers[depth], depth + 1);
     }
-
-    DFS(idx + 1, sum + numbers[idx]);
-    DFS(idx + 1, sum - numbers[idx]);
-  };
-
-  DFS(0, 0);
-
-  return ans;
+    
+    DFS(0, 0)
+    
+    return ans;
 }
