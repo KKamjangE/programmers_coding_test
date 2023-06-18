89cb49f727896a2ba8006d99217e31fa703e89df
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-06-18 18:36:40 +0900

    [level 3] Title: 가장 먼 노드, Time: 312.77 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,29 +1,24 @@
-function solution(n, edge) {
-  const graph = Array.from({ length: n + 1 }, () => []); // 간선 그래프
-  const countArr = new Array(n + 1).fill(0); // 도착 결과 배열
-  countArr[1] = 1; // 1번 노드 초기화
-
-  // 양방향 간선 그래프
-  edge.forEach(([a, b]) => {
-    graph[a].push(b);
-    graph[b].push(a);
-  });
-
-  const que = [[1, 1]]; // Queue 자료구조 [노드 번호, 거리]
-
-  while (que.length > 0) {
-    [node, depth] = que.shift(); // [노드 번호, 거리]
-
-    // 해당 노드의 간선 순회
-    graph[node].forEach((value) => {
-      // 거리가 아직 입력되지 않았다면
-      if (countArr[value] === 0) {
-        que.push([value, depth + 1]);
-        countArr[value] = depth + 1;
-      }
+function solution(n, edge) {    
+    const graph = Array.from({length: n + 1}, () => new Array);
+    edge.forEach(([a, b]) => {
+        graph[a].push(b);
+        graph[b].push(a);
     });
-  }
-
-  const maxDepth = Math.max(...countArr);
-  return countArr.filter((value) => value === maxDepth).length;
+    const visited = new Array(n + 1).fill(0);
+    visited[1] = 1;
+    const que = [[1, 0]];
+    
+    while(que.length > 0){
+        const [q, count] = que.shift();
+        
+        graph[q].forEach((v) => {
+            if(visited[v] === 0){
+                visited[v] = count + 1;
+                que.push([v, count + 1]);
+            }
+        });
+    }
+    
+    const maxNum = Math.max(...visited);
+    return visited.filter((value) => value === maxNum).length;
 }
