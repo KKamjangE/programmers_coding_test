c62b41d64f5c28ba814653417c7daf074b97ea68
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-06-18 11:57:52 +0900

    [level 1] Title: K번째수, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,12 +1,11 @@
 function solution(array, commands) {
-  var ans = [];
-
-  commands.forEach((command) => {
-    [i, j, k] = command;
-    newArr = array.slice(i - 1, j);
-    newArr.sort((a, b) => a - b);
-    ans.push(newArr[k - 1]);
-  });
-
-  return ans;
+    let ans = [];
+    
+    commands.forEach(([start, end, idx]) => {
+        let tmp = array.slice(start - 1, end);
+        tmp.sort((a, b) => a - b);
+        ans.push(tmp[idx - 1])
+    })
+    
+    return ans;
 }
