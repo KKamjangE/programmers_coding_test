6dd33747fa3b09a6f145920cfe4386047a550450
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-06-18 19:05:49 +0900

    [level 3] Title: 입국심사, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.js
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.js
@@ -1,26 +1,23 @@
 function solution(n, times) {
-  let ans = 0;
-  let right = Math.max(...times) * n;
-  let left = 1;
-
-  while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
-    let people = 0;
-
-    for (let i = 0; i < times.length; i++) {
-      people += Math.floor(mid / times[i]);
-      if (people >= n) {
-        break;
-      }
+    let ans = 0;
+    
+    let left = 0;
+    let right = Math.max(...times) * n;
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2);
+        let people = 0;
+        for(let i = 0; i < times.length; i++){
+            people += Math.floor(mid / times[i]);
+            if(people >= n) break;
+        }
+        
+        if(people >= n){
+            ans = mid;
+            right = mid - 1;
+        } else if(people < n) {
+            left = mid + 1;
+        }
     }
-
-    if (people >= n) {
-      ans = mid;
-      right = mid - 1;
-    } else {
-      left = mid + 1;
-    }
-  }
-
-  return ans;
+    
+    return ans;
 }
