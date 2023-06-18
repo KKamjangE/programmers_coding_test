3f910c14ebca80983002ad7243f854dcf3311031
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-06-18 16:04:01 +0900

    [level 2] Title: 모음 사전, Time: 0.51 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
@@ -1,26 +1,25 @@
 function solution(word) {
-  let ans = 0;
-  let alpha = ["A", "E", "I", "O", "U"];
-  let count = 0;
-  let flg = false;
-
-  const DFS = (str) => {
-    if (flg) return;
-    if (str.length > 5) return;
-
-    if (str === word) {
-      ans = count;
-      flg = true;
-      return;
+    let ans = 0;
+    let flg = false;
+    let count = 0;
+    const alpha = ["A", "E", "I", "O", "U"];
+    
+    const DFS = (str) => {
+        if(flg || str.length > 5) return;
+        
+        if(str === word){
+            ans = count;
+            flg = true;
+            return;
+        }
+        
+        count += 1;
+        for(let i = 0; i < alpha.length; i++){
+            DFS(str + alpha[i]);
+        }
     }
-    count += 1;
-
-    for (let i = 0; i < 5; i++) {
-      DFS(str + alpha[i]);
-    }
-  };
-
-  DFS("");
-
-  return ans;
+    
+    DFS("");
+    
+    return ans;
 }
