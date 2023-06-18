e9ad11410f16a67aa7f28efffa4a61a9ac8d29e7
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-06-18 16:45:39 +0900

    [level 2] Title: 조이스틱, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42860. 조이스틱/조이스틱.js
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.js
@@ -1,23 +1,19 @@
 function solution(name) {
-  let ans = 0;
-  let move = name.length - 1;
-
-  [...name].forEach((value, i) => {
-    let code = value.charCodeAt();
-    ans += Math.min(code - 65, 91 - code);
-
-    let idx = i + 1;
-
-    while (idx < name.length && name[idx] === "A") {
-      idx += 1;
-    }
-
-    move = Math.min(
-      move,
-      i * 2 + name.length - idx,
-      i + 2 * (name.length - idx)
-    );
-  });
-
-  return ans + move;
+    let ans = 0;
+    let move = name.length - 1; // 조이스틱 좌우 이동
+    
+    [...name].forEach((str, i) => {
+        let code = str.charCodeAt(); // 현재 문자의 아스키코드
+        ans += Math.min(code - 65, 91 - code); // 조이스틱 상하 이동
+        
+        let idx = i + 1; // 다음 문자로 조이스틱 이동
+        
+        while(name[idx] === "A" && idx < name.length){
+            idx += 1;
+        }
+        
+        move = Math.min(move, i * 2 + name.length - idx, i + 2 * (name.length - idx));
+    });
+    
+    return ans + move;
 }
