7f73d255803db0c183246944acaec45795384438
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-06-18 14:26:17 +0900

    [level 1] Title: 모의고사, Time: 1.64 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,25 +1,19 @@
 function solution(answers) {
-  let ans = [];
-  let a1 = [1, 2, 3, 4, 5];
-  let a2 = [2, 1, 2, 3, 2, 4, 2, 5];
-  let a3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-
-  let a1c = answers.filter(
-    (value, idx) => value === a1[idx % a1.length]
-  ).length;
-  let a2c = answers.filter(
-    (value, idx) => value === a2[idx % a2.length]
-  ).length;
-  let a3c = answers.filter(
-    (value, idx) => value === a3[idx % a3.length]
-  ).length;
-  let max = Math.max(a1c, a2c, a3c);
-
-  [a1c, a2c, a3c].forEach((value, idx) => {
-    if (value === max) {
-      ans.push(idx + 1);
-    }
-  });
-
-  return ans;
+    let ans = [0, 0, 0];
+    const case1 = [1, 2, 3, 4, 5];
+    const case2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    const case3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    
+    answers.forEach((value, idx) => {
+        if(case1[idx % case1.length] === value) ans[0] += 1;
+        if(case2[idx % case2.length] === value) ans[1] += 1;
+        if(case3[idx % case3.length] === value) ans[2] += 1;
+    })
+    
+    const maxNum = Math.max(...ans);
+    
+    return ans.reduce((prev, curr, idx) => {
+        if(curr === maxNum) return [...prev, idx + 1];
+        else return prev;
+    }, []);
 }
