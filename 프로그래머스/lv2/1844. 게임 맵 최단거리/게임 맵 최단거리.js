0a8cd0e37c7e1d7ebeba0351195f0db10ca77148
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-06-18 18:23:37 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.09 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,41 +1,27 @@
 function solution(maps) {
-  let ans = 0;
-  const M = maps.length;
-  const N = maps[0].length;
-
-  let [dx, dy] = [
-    [1, -1, 0, 0],
-    [0, 0, 1, -1],
-  ];
-
-  let visited = Array.from({ length: M }, () =>
-    new Array(maps[0].length).fill(false)
-  );
-
-  let que = [[0, 0, 1]];
-
-  while (que.length > 0) {
-    [x, y, depth] = que.shift();
-    if (M - 1 === x && N - 1 === y) {
-      ans = depth;
-      break;
+    const visited = Array.from({length: maps.length}, () => new Array(maps[0].length).fill(-1));
+    
+    const BFS = () => {
+        const que = [[0, 0, 0]];
+        visited[0][0] = 1;
+        const dx = [0, 0, 1, -1];
+        const dy = [1, -1, 0, 0];
+        
+        while(que.length > 0){
+            let [x, y] = que.shift();
+            
+            for(let i = 0; i < 4; i++){
+                let nx = x + dx[i];
+                let ny = y + dy[i];
+                if(0 <= nx && nx < maps[0].length && 0 <= ny && ny < maps.length && visited[ny][nx] === -1 && maps[ny][nx] === 1){
+                    visited[ny][nx] = visited[y][x] + 1;
+                    que.push([nx, ny]);
+                }
+            }
+        }
     }
-    for (let i = 0; i < 4; i++) {
-      let nx = x + dx[i];
-      let ny = y + dy[i];
-      if (
-        0 <= nx &&
-        nx < M &&
-        0 <= ny &&
-        ny < N &&
-        maps[nx][ny] === 1 &&
-        !visited[nx][ny]
-      ) {
-        visited[nx][ny] = true;
-        que.push([nx, ny, depth + 1]);
-      }
-    }
-  }
-
-  return ans ? ans : -1;
+    
+    BFS();
+    
+    return visited[maps.length - 1][maps[0].length - 1];
 }
