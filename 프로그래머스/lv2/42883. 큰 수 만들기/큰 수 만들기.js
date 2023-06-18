c248f494eef323499260ddbfdd7d6e8599dcd8c3
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-06-18 16:59:50 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,11 +1,14 @@
 function solution(number, k) {
-  var ans = [];
-  for (i of number) {
-    while (k > 0 && ans && ans[ans.length - 1] < i) {
-      ans.pop();
-      k--;
-    }
-    ans.push(i);
-  }
-  return ans.slice(0, ans.length - k).join("");
+    const ans = [];
+    
+    [...number].forEach((num) => {
+        while(k > 0 && ans && ans[ans.length - 1] < num){
+            ans.pop();
+            k -= 1;
+        }
+        ans.push(num);    
+    });
+    
+    
+    return ans.slice(0, ans.length - k).join("");
 }
