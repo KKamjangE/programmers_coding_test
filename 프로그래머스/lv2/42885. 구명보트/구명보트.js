7dc945962ee982b885c07175f4ee4c52bff66088
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-06-18 17:12:48 +0900

    [level 2] Title: 구명보트, Time: 26.68 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,16 +1,17 @@
 function solution(people, limit) {
-  let ans = 0;
-  people.sort((a, b) => a - b);
-  left = 0;
-  right = people.length - 1;
-  while (left < right) {
-    if (people[left] + people[right] <= limit) {
-      ans++;
-      left++;
-      right--;
-    } else {
-      right--;
+    let ans = people.length; // 최대 보트 사용 수로 초기화
+    people.sort((a, b) => a - b); // 오름차순 정렬
+
+    let left = 0; // 몸무게가 제일 낮은 사람
+    let right = people.length - 1; // 몸무게가 제일 높은 사람
+    
+    while(left < right){
+        if(people[left] + people[right] <= limit){ // 2명이 함께 탈 수 있다면 몸무게 낮은 사람도 포함
+            left += 1;
+            ans -= 1; // 2명이 함께 보트를 사용헀다면 보트 사용 횟수 -1
+        } // 어떤 경우에도 몸무게 높은 사람은 포함
+        right -= 1;
     }
-  }
-  return people.length - ans;
+    
+    return ans;
 }
