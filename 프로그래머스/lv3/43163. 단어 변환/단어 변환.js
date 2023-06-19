d084e15d15e33d2ad0474c647802cf9b208f92d6
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-06-19 12:58:32 +0900

    [level 3] Title: 단어 변환, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
@@ -1,32 +1,34 @@
 function solution(begin, target, words) {
-  var ans = 0;
-  visited = Array.from({ length: words.length }, (_) => false);
-
-  const bfs = (word, start) => {
-    que = [[word, start]];
-
-    while (que.length > 0) {
-      let [q, count] = que.shift();
-      if (q === target) {
-        ans = count;
-        break;
-      }
-      for (i in words) {
-        let tmp = 0;
-        for (let j = 0; j < words[i].length; j++) {
-          if (q[j] != words[i][j]) {
-            tmp += 1;
-          }
+    let ans = 0;
+    if(!words.includes(target)) return 0; // 문자열 배열안에 target이 없다면 0
+    
+    const visited = new Array(words.length).fill(false); // 방문 처리
+    
+    const BFS = (word, start) => {
+        que = [[word, start]]; // 시작 문자열, 단계
+        
+        while(que.length > 0){
+            const [q, count] = que.shift();
+            if(q === target){ // 현재 문자열이 target과 같다면
+                ans = count; // 몇 단계인지 저장
+                break;
+            }
+            words.forEach((word, idx) => { // 문자열 배열 순회
+                let diff = 0; // 다른 문자 개수
+                for(let i = 0; i < q.length; i++){ // 문자열 순회
+                    if(word[i] != q[i]){ // 현재 문자열과 문자가 다른지 비교
+                        diff += 1;
+                    }
+                }
+                if(diff === 1 && !visited[idx]){ // 다른 문자가 1개라면
+                    visited[idx] = true; // 방문 처리
+                    que.push([word, count + 1]); // 문자열, 단계 + 1
+                }
+            });
         }
-        if (tmp === 1 && visited[i] === false) {
-          visited[i] = true;
-          que.push([words[i], count + 1]);
-        }
-      }
     }
-  };
-
-  bfs(begin, 0);
-
-  return ans;
+    
+    BFS(begin, 0);
+    
+    return ans;
 }
