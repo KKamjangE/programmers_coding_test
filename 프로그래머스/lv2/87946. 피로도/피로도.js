d4e4969b43987a2908f1d2986f0f451db5cb54b3
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-07-05 21:15:05 +0900

    [level 2] Title: 피로도, Time: 0.31 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.js
+++ 프로그래머스/lv2/87946. 피로도/피로도.js
@@ -1,20 +1,21 @@
 function solution(k, dungeons) {
-    let ans = 0;
-    const visited = new Array(dungeons.length).fill(false);
-    
-    const DFS = (currK, count) => {
-        if(ans < count) ans = count;
-        
-        dungeons.forEach(([minK, conK], idx) => {
-            if(minK <= currK && !visited[idx]){
-                visited[idx] = true;
-                DFS(currK - conK, count + 1);
-                visited[idx] = false;
-            }
-        })
-    }
-    
-    DFS(k, 0);
-    
-    return ans;
+  let ans = 0;
+  const visited = new Array(dungeons.length).fill(false); // 방문 처리 배열
+
+  const DFS = (k, count) => {
+    if (ans < count) ans = count;
+
+    dungeons.forEach(([minK, conK], idx) => {
+      if (minK <= k && !visited[idx]) {
+        // 입장 가능한 피로도이며 방문하지 않았다면
+        visited[idx] = true; // 방문 처리
+        DFS(k - conK, count + 1); // 피로도 - 소모피로도, 탐험한 던전 수 + 1
+        visited[idx] = false; // DFS를 나왔으므로 방문해제
+      }
+    });
+  };
+
+  DFS(k, 0); // 피로도, 탐험한 던전 수
+
+  return ans;
 }
