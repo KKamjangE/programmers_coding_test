b1b25742c90a12cd061fcac5eae55a5aa3ed7d69
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-07-05 21:02:58 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.36 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,35 +1,39 @@
 function solution(n, wires) {
-    let ans = n;
-    let graph = Array.from({length: n + 1}, () => new Array);
+  let ans = n; // 최대 길이로 초기화
+  const graph = Array.from({ length: n + 1 }, () => new Array()); // 빈 그래프 배열
 
-    const BFS = (start, except) => {
-        const que = [[start]];
-        const visited = new Array(n + 1).fill(false);
-        let count = 1;
-        visited[start] = true;
+  wires.forEach(([v1, v2]) => {
+    // 양방향 간선
+    graph[v1].push(v2);
+    graph[v2].push(v1);
+  });
 
-        while(que.length > 0){
-            q = que.shift();
-            graph[q].forEach((v) => {
-                if(v != except && !visited[v]){
-                    visited[v] = true;
-                    que.push(v);
-                    count += 1;
-                }
-            })
+  const BFS = (start, except) => {
+    let que = [start];
+    const visited = new Array(n + 1).fill(false); // 방문 처리 배열
+    visited[start] = true;
+    let count = 1; // 시작 노드부터 카운트
+
+    while (que.length > 0) {
+      const q = que.shift();
+
+      graph[q].forEach((v) => {
+        if (v != except && !visited[v]) {
+          // 끊어진 부분이 아니고 방문하지 않았다면
+          que.push(v);
+          visited[v] = true;
+          count += 1; // 노드 개수 + 1
         }
-        
-        return count;
+      });
     }
-    
-    wires.forEach(([v1, v2]) => {
-        graph[v1].push(v2);
-        graph[v2].push(v1);
-    })
-    
-    wires.forEach(([a, b]) => {
-        ans = Math.min(ans, Math.abs(BFS(a, b) - BFS(b, a)));
-    })
-    
-    return ans;
+    return count; // 노드 개수 반환
+  };
+
+  wires.forEach(([v1, v2]) => {
+    // 주어진 간선들 순회
+    // BFS(시작 노드, 끊어진 노드)
+    ans = Math.min(ans, Math.abs(BFS(v1, v2) - BFS(v2, v1)));
+  });
+
+  return ans;
 }
