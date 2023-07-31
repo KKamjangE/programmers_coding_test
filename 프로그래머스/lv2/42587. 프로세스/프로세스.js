18c95be41e76996c1a180ac5f81e7a03b88e4a9e
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-07-31 23:05:50 +0900

    [level 2] Title: 프로세스, Time: 0.34 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.js
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.js
@@ -1,15 +1,15 @@
 function solution(priorities, location) {
     const ans = [];
-    const stack = priorities.map((value, index) => [value, index]);
-
-    while (stack.length > 0) {
-        let tmp = stack.shift();
-        if (stack.length > 0 && stack.some((ele) => ele[0] > tmp[0])) {
-            stack.push(tmp);
+    const que = priorities.map((value, index) => [value, index]); // 우선순위와 프로세스 순서 저장
+    
+    while(que.length){
+        const temp = que.shift(); // 프로세스 하나 꺼냄
+        if(que.length && que.some((ele) => ele[0] > temp[0])){ // 프로세스의 우선순위가 제일 높은지 확인
+            que.push(temp); // 우선순위가 밀린다면 다시 큐에 추가
         } else {
-            ans.push(tmp);
+            ans.push(temp); // 우선순위가 제일 높다면 실행
         }
     }
     
-    return ans.findIndex((ele) => ele[1] === location) + 1;
+    return ans.findIndex((ele) => ele[1] === location) + 1; // 인덱스이기 때문에 + 1
 }
