0ea0314b1ccdfb09dffe6d629d14ec9dbbffbab2
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-01 16:40:28 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,13 +1,12 @@
 function solution(brown, yellow) {
-  let height = 1;
-  let width = yellow / height;
-
-  while (width >= height) {
-    if (brown === (width + 2) * 2 + height * 2) {
-      return [width + 2, height + 2];
-    } else {
-      height += 1;
-      width = yellow / height;
+    let height = 1; // 높이 1부터 시작
+    let width = yellow / height; // 높이에 따른 yellow 넓이 설정
+    while(width >= height) { // 높이가 넓이를 넘어가지 않을 때
+        if(brown === (width + 2) * 2 + height * 2){
+            return [width + 2, height + 2];
+        } else {
+            height += 1; // 높이 + 1
+            width = yellow / height; // 높이 변화에 따른 넓이 수정
+        }
     }
-  }
 }
