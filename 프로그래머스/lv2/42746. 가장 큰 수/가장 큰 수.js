c25458225c1d0ce98737f66513886de30a6b2ae7
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-01 14:12:19 +0900

    [level 2] Title: 가장 큰 수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,4 +1,10 @@
 function solution(numbers) {
-    const ans = numbers.map((num) => num + "").sort((a, b) => b + a - (a + b)).join("");
-    return ans[0] === "0" ? "0" : ans;
+  const ans = numbers.map((num) => num + ""); // 문자열로 변환
+  ans.sort((a, b) => b + a - (a + b)); // 두 요소를 조합했을 때 더 큰 수로 정렬
+
+  if (ans[0] == "0") {
+    return "0"; // 맨 앞의 수가 0이라면 "0" 출력
+  } else {
+    return ans.join(""); // 배열 합쳐서 출력
+  }
 }
