83c87c3cdf5f01b7d0d68fe309b8ae380c4abe55
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-01 14:24:37 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -1,15 +1,15 @@
 function solution(citations) {
-  let ans = 0;
-
-  citations.sort((a, b) => b - a);
-
-  for (let i = 0; i < citations.length; i++) {
-    if (i < citations[i]) {
-      ans += 1;
-    } else if (i > citations[i]) {
-      break;
+    let ans = 0;
+    
+    citations.sort((a, b) => b - a); // 내림차순 정렬
+    
+    for(let h = 0; h < citations.length; h++) {
+        if(h + 1 <= citations[h]) { // h번 이상 인용된 논문이 h편 이상인지
+            ans += 1;
+        } else {
+            break;
+        }
     }
-  }
-
-  return ans;
+    
+    return ans;
 }
