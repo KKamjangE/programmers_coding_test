804064d744661042fa615e2d79852a5a714201bd
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-01 15:58:49 +0900

    [level 2] Title: 소수 찾기, Time: 0.38 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
@@ -1,34 +1,29 @@
 function solution(numbers) {
-  let ans = new Set(); // 중복 제거용 Set
-
-  const prime = (num) => {
-    // 소수 판별 함수
-    if (num < 2) return false;
-
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-      if (num % i === 0) return false;
+    const ans = new Set();
+    
+    const isPrime = (num) => {
+        if(num < 2) return false;
+        for(let i = 2; i <= Math.sqrt(num); i++){
+            if(num % i === 0) return false;
+        }
+        return true;
     }
-
-    return true;
-  };
-
-  const DFS = (numArr, fixNum) => {
-    if (numArr.length === 0) return; // 남은 숫자가 더이상 없다면 return
-
-    for (let i = 0; i < numArr.length; i++) {
-      // 숫자 배열 순회
-      if (prime(parseInt(fixNum + numArr[i]))) {
-        // 소수 인지 판별해서 맞다면 ans에 추가
-        ans.add(parseInt(fixNum + numArr[i]));
-      }
-
-      const tmp = [...numArr]; // 현재 문자 배열 복사
-      tmp.splice(i, 1); // 숫자 배열 중 현재 숫자 제거
-      DFS(tmp, fixNum + numArr[i]); // 남은 숫자 배열, 현재 숫자 고정
-    }
-  };
-
-  DFS(numbers.split(""), "");
-
-  return ans.size; // Set에 담긴 소수 개수 반환
+    
+    const DFS = (numArr, fixNum) => {
+        if(numArr.length === 0) return;
+        
+        for(let i = 0; i < numArr.length; i++){
+            if(isPrime(parseInt(fixNum + numArr[i]))) {
+                ans.add(parseInt(fixNum + numArr[i]));
+            }
+            
+            const temp = [...numArr];
+            temp.splice(i, 1);
+            DFS(temp, fixNum + numArr[i]);
+        }
+    };
+    
+    DFS(numbers.split(""), "");
+    
+    return ans.size;
 }
