da8b739c1fd97b2bc59f5b239d00522fa41e54cf
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-01 15:23:48 +0900

    [level 1] Title: 모의고사, Time: 3.08 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,20 +1,27 @@
 function solution(answers) {
-  let ans = [0, 0, 0];
+    // 수포자 패턴
+    const patterns = [
+        [1, 2, 3, 4, 5],
+        [2, 1, 2, 3, 2, 4, 2, 5],
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+    ];
+    const ans = [0, 0, 0];
 
-  const case1 = [1, 2, 3, 4, 5];
-  const case2 = [2, 1, 2, 3, 2, 4, 2, 5];
-  const case3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    answers.forEach((value, index) => {
+        patterns.forEach((pattern, patternIndex) => {
+            if (pattern[index % pattern.length] === value) {
+                // 맞춘 문제 수
+                ans[patternIndex] += 1;
+            }
+        });
+    });
 
-  answers.forEach((value, idx) => {
-    if (value === case1[idx % case1.length]) ans[0] += 1;
-    if (value === case2[idx % case2.length]) ans[1] += 1;
-    if (value === case3[idx % case3.length]) ans[2] += 1;
-  });
+    const maxNum = Math.max(...ans); // 최대 정답 수
 
-  const maxNum = Math.max(...ans);
-
-  return ans.reduce((prev, curr, idx) => {
-    if (curr === maxNum) return [...prev, (idx += 1)];
-    else return prev;
-  }, []);
+    return ans.reduce((result, item, index) => {
+        if (item === maxNum) {
+            result.push(index + 1);
+        }
+        return result;
+    }, []);
 }
