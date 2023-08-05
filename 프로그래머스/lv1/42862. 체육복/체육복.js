a34ac3294a1d03bb929fd955c4dbfdd2085fc8c3
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-05 20:27:14 +0900

    [level 1] Title: 체육복, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,21 +1,30 @@
 function solution(n, lost, reserve) {
-    const arr = new Array(n + 1).fill(0);
-    lost.forEach((idx) => {
-        arr[idx] -= 1;
-    })
-    reserve.forEach((idx) => {
-        arr[idx] += 1;
-    })
-
-    for(let i = 2; i < arr.length; i++){
-        if (arr[i] === -1 && arr[i-1] === 1){
-            arr[i] = 0;
-            arr[i-1] = 0;
-        }else if (arr[i] === 1 && arr[i-1] === -1){
-            arr[i] = 0;
-            arr[i-1] = 0;
+    const child = new Array(n + 1).fill(0); // 학생 배열
+    // 앞 사람부터 체육복을 나눠주어야 하기 때문에 index + 1
+    reserve.forEach((value) => {
+        // 체육복을 하나 더 가지고 있는 학생은 + 1
+        child[value] += 1;
+    });
+    lost.forEach((value) => {
+        // 체육복이 없는 학생은 - 1
+        child[value] -= 1;
+    });
+    
+    for(let i = 1; i < child.length; i++){
+        if(child[i] === 1){ // 현재 학생이 체육복을 하나 더 가지고 있다면
+            if(child[i - 1] < 0) { // 앞사람 확인
+                child[i] = 0;
+                child[i - 1] = 0;
+            } else if (child[i + 1] < 0){ // 뒷사람 확인
+                child[i] = 0;
+                child[i + 1] = 0;
+            }
         }
     }
     
-    return arr.filter((val) => val >= 0).length - 1;
+    // 체육복이 있는 학생들 수 계산 배열을 index + 1로 만들었기 때문에 초기값을 -1로 설정
+    return child.reduce((result, item) => {
+        if(item >= 0) result += 1;
+        return result;
+    }, -1);
 }
