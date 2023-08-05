6dfcb82dbf927b028ba3966e9f56fb650cac2c57
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-05 19:58:23 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.39 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,34 +1,38 @@
 function solution(n, wires) {
     let ans = n;
-
-    const graph = Array.from({length: n + 1}, () => new Array);
+    const graph = Array.from({length: n + 1}, () => new Array); // 빈 그래프 생성
+    
     wires.forEach(([v1, v2]) => {
+        // 양방향 그래프 입력
         graph[v1].push(v2);
         graph[v2].push(v1);
-    })
-
+    });
+    
     const BFS = (start, except) => {
-        const que = [start];
-        const visited = new Array(n + 1).fill(false);
-        visited[start] = true;
-        let count = 1;
+        const visited = new Array(n + 1).fill(false); // 방문 처리 배열
+        const que = [start]; // 시작 노드
+        visited[start] = true; // 시작 노드 방문 처리
+        let count = 0; // 노드 개수
         
         while(que.length > 0) {
-            q = que.shift();
+            const q = que.shift();
             graph[q].forEach((value) => {
-                if(!visited[value] && value !== except) {
-                    visited[value] = true;
+                if(!visited[value] && except !== value) { // 방문하지 않았고 끊어진 노드가 아니라면
                     que.push(value);
-                    count += 1;
+                    visited[value] = true;
+                    count += 1; // 노드 개수 + 1
                 }
             });
         }
-        return count;
+        
+        return count; // 노드 개수 반환
     }
     
     wires.forEach(([v1, v2]) => {
+        // 두 전력망의 차이가 가장 적은 수
+        // BFS(시작 노드, 끊어진 노드)
         ans = Math.min(ans, Math.abs(BFS(v1, v2) - BFS(v2, v1)));
-    })
+    });
     
     return ans;
 }
