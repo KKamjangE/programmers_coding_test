531f2220ecb87973c69262e0798060eae04dc704
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-05 19:44:37 +0900

    [level 2] Title: 모음 사전, Time: 0.53 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
@@ -1,25 +1,24 @@
 function solution(word) {
     let ans = 0;
-    let flg = false;
-    let count = 0;
-    const alpha = ["A", "E", "I", "O", "U"];
+    let flg = false; // DFS 중지 플래그
+    const alpha = ["A", "E", "I", "O", "U"]; // 사전 배열
     
     const DFS = (str) => {
-        if(flg || str.length > 5) return;
+        if(flg || str.length > 5) return; // 중지 플래그가 true거나 문자열 길이가 5 이상이라면 return
         
-        if(str === word){
-            ans = count;
-            flg = true;
+        if(word === str) {
+            // 사전의 단어와 동일하다면
+            flg = true; // DFS 중지
             return;
         }
-        
-        count += 1;
+        ans += 1; // 사전 순서 + 1
         for(let i = 0; i < alpha.length; i++){
+            // alpha배열 순회하면서 재귀 
             DFS(str + alpha[i]);
         }
     }
     
-    DFS("");
+    DFS("")
     
     return ans;
 }
