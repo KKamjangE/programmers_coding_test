b7f7dae96667c0c37676978827d4c86e53f5d89a
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-07 20:23:47 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.22 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,27 +1,25 @@
 function solution(maps) {
-    const visited = Array.from({length: maps.length}, () => new Array(maps[0].length).fill(-1));
+    const maxY = maps.length; // 최대 y 값
+    const maxX = maps[0].length; // 최대 x 값
+    const dx = [0, 0, 1, -1]; // x 이동 값
+    const dy = [1, -1, 0, 0]; // y 이동 값
+    const visited = Array.from({length: maxY}, () => new Array(maxX).fill(-1)); // 방문 처리 배열 목적지에 도달할 수 없는 경우 -1을 반환해야하기 때문에 초기값을 -1로 지정
     
-    const BFS = () => {
-        const que = [[0, 0, 0]];
-        visited[0][0] = 1;
-        const dx = [0, 0, 1, -1];
-        const dy = [1, -1, 0, 0];
-        
-        while(que.length > 0){
-            let [x, y] = que.shift();
-            
-            for(let i = 0; i < 4; i++){
-                let nx = x + dx[i];
-                let ny = y + dy[i];
-                if(0 <= nx && nx < maps[0].length && 0 <= ny && ny < maps.length && visited[ny][nx] === -1 && maps[ny][nx] === 1){
-                    visited[ny][nx] = visited[y][x] + 1;
-                    que.push([nx, ny]);
-                }
+    const que = [[0,0]]; // 시작 좌표
+    visited[0][0] = 1; // 시작 좌표값 1로 초기화
+    
+    while(que.length > 0) {
+        const [y, x] = que.shift();
+        for(let i = 0; i < 4; i++) {
+            const nx = x + dx[i];
+            const ny = y + dy[i];
+            if(0 <= nx && nx < maxX && 0 <= ny && ny < maxY && visited[ny][nx] === -1 && maps[ny][nx] === 1) {
+                // 최대 좌표를 넘지 않고 방문하지 않았고 벽으로 막혀있지 않다면
+                que.push([ny, nx]); // 다음 좌표값 저장
+                visited[ny][nx] = visited[y][x] + 1; // 현재 좌표값에서 + 1
             }
         }
     }
     
-    BFS();
-    
-    return visited[maps.length - 1][maps[0].length - 1];
+    return visited[maxY - 1][maxX - 1]; // 목적지 값 반환
 }
