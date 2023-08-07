36c8905d70a799f126fd43d96ee534f4e57c5663
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-07 14:21:04 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,14 +1,17 @@
 function solution(number, k) {
     const ans = [];
     
-    [...number].forEach((num) => {
-        while(k > 0 && ans && ans[ans.length - 1] < num){
-            ans.pop();
+    number.split("").forEach((num) => {
+        while(k > 0 && ans.length > 0 && ans[ans.length - 1] < num){
+            // k가 남았고 ans의 마지막 값보다 현재 num이 클 때
+            ans.pop(); // 마지막 값 제거
             k -= 1;
         }
-        ans.push(num);    
+        ans.push(num); // 현재 요소 추가
     });
     
+    // k가 남아있다면 k개 만큼 맨 뒤 숫자 제거
+    if(k > 0) ans.splice(ans.length - k);
     
-    return ans.slice(0, ans.length - k).join("");
+    return ans.join(""); // 배열 문자열로 변환해서 반환
 }
