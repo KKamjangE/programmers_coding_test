b96f7e461a650e2c85e601aa2e8423b8391d8ef7
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-07 23:10:13 +0900

    [level 3] Title: 가장 먼 노드, Time: 63.36 ms, Memory: 55.7 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,24 +1,26 @@
-function solution(n, edge) {    
-    const graph = Array.from({length: n + 1}, () => new Array);
-    edge.forEach(([a, b]) => {
+function solution(n, edge) {
+    const visited = new Array(n + 1).fill(0); // 방문 처리 배열
+    const graph = Array.from({length: n + 1}, () => new Array()); // 간선 그래프 생성
+    
+    edge.forEach(([a, b]) => { // 간선 그래프 초기화
         graph[a].push(b);
         graph[b].push(a);
     });
-    const visited = new Array(n + 1).fill(0);
-    visited[1] = 1;
-    const que = [[1, 0]];
     
-    while(que.length > 0){
+    const que = [[1, 1]]; // 시작 노드, 거리
+    visited[1] = 1 // 시작 노드 위치 1로 초기화
+    
+    while(que.length > 0) {
         const [q, count] = que.shift();
         
-        graph[q].forEach((v) => {
-            if(visited[v] === 0){
-                visited[v] = count + 1;
-                que.push([v, count + 1]);
+        graph[q].forEach((value) => {
+            if(visited[value] === 0){ // 방문하지 않았다면
+                visited[value] = count + 1; // 현재 위치에서 + 1
+                que.push([value, count + 1]);
             }
-        });
+        })
     }
     
-    const maxNum = Math.max(...visited);
-    return visited.filter((value) => value === maxNum).length;
+    const max = Math.max(...visited); // 가장 먼 노드의 위치 값
+    return visited.filter((value) => value === max).length; // 가장 먼 노드가 몇 개인지 반환
 }
