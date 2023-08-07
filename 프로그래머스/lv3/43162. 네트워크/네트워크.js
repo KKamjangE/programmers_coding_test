717a77a7bab93c77bbd69081349fcbc18856a6e2
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-07 19:27:40 +0900

    [level 3] Title: 네트워크, Time: 0.85 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.js
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.js
@@ -1,28 +1,32 @@
 function solution(n, computers) {
-  let ans = 0;
-
-  const visited = Array(n).fill(false);
-
-  const BFS = (start) => {
-    const que = [start];
-
-    while (que.length > 0) {
-      q = que.shift();
-      computers[q].forEach((value, idx) => {
-        if (!visited[idx] && value === 1) {
-          visited[idx] = true;
-          que.push(idx);
+    let ans = 0;
+    
+    const visited = new Array(n).fill(false); // 방문 처리 배열
+    
+    const BFS = (start) => {
+        const que = [start];
+        visited[start] = true;
+        
+        while(que.length > 0){
+            const q = que.shift();
+            computers[q].forEach((value, index) => {
+                if(!visited[index] && value === 1){ // 방문 하지 않았고 연결되어 있다면
+                    que.push(index);
+                    visited[index] = true; // 방문처리
+                }
+            })
         }
-      });
+        return 1; // 네트워크 + 1
     }
-  };
-
-  for (let i = 0; i < n; i++) {
-    if (!visited[i]) {
-      BFS(i);
-      ans += 1;
+    
+    for(const computer of computers){
+        computer.forEach((value, index) => {
+            if(!visited[index] && value === 1){
+                // 방문하지 않았고 다른 컴퓨터와 연결되어 있다면
+                ans += BFS(index);
+            }
+        });
     }
-  }
-
-  return ans;
+    
+    return ans;
 }
