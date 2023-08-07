b4c5e93f2615cd9ffe013fcde37c64003c9d2dcc
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-07 18:07:28 +0900

    [level 2] Title: 타겟 넘버, Time: 1.08 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,17 +1,20 @@
 function solution(numbers, target) {
-  let ans = 0;
-
-  const DFS = (num, depth) => {
-    if (depth === numbers.length) {
-      if (num === target) ans += 1;
-      return;
+    let ans = 0;
+    
+    const DFS = (count, depth) => {
+        if(numbers.length === depth) {
+            // numbers 배열의 마지막 인덱스까지 도달했다면
+            if(count === target){
+                // 현재 숫자가 타겟과 같다면
+                ans += 1;
+            }
+            return;
+        }
+        DFS(count - numbers[depth], depth + 1); // 현재 값에서 뺀 경우
+        DFS(count + numbers[depth], depth + 1); // 현재 값에서 더한 경우
     }
-
-    DFS(num + numbers[depth], depth + 1);
-    DFS(num - numbers[depth], depth + 1);
-  };
-
-  DFS(0, 0);
-
-  return ans;
+    
+    DFS(0, 0);
+    
+    return ans;
 }
