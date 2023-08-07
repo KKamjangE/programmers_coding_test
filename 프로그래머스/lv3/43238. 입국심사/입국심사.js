c53af2b72494569cdfa14aed5285c19ac8c77d5c
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-07 21:37:53 +0900

    [level 3] Title: 입국심사, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.js
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.js
@@ -1,21 +1,22 @@
 function solution(n, times) {
     let ans = 0;
+    let left = 0; // 최소 시간
+    let right = n * Math.max(...times); // 최대 시간
     
-    let left = 0;
-    let right = Math.max(...times) * n;
-    while(left <= right){
-        let mid = Math.floor((left + right) / 2);
-        let people = 0;
+    while(left <= right) {
+        let mid = Math.floor((left + right) / 2); // 임의의 주어진 시간
+        let people = 0; // 입국심사 가능한 인원
+        
         for(let i = 0; i < times.length; i++){
-            people += Math.floor(mid / times[i]);
-            if(people >= n) break;
+            people += Math.floor(mid / times[i]); // 시간 내에 입국심사 가능한 인원
+            if(people >= n) break; // 대기 인원보다 많으면 시간이 남는다는 뜻
         }
         
-        if(people >= n){
-            ans = mid;
-            right = mid - 1;
-        } else if(people < n) {
-            left = mid + 1;
+        if(people >= n) { // 시간이 남는다면
+            ans = mid; // 시간 내에 가능하다는 뜻
+            right = mid - 1; // 최대 시간 줄이기
+        } else if (people < n) { // 시간이 부족하다면
+            left = mid + 1; // 최소 시간 늘리기
         }
     }
     
