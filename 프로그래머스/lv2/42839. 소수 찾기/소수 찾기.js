a1be769470ab04ce69a605709ed1931ad41bce96
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-09 15:14:09 +0900

    [level 2] Title: 소수 찾기, Time: 0.38 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
@@ -1,29 +1,30 @@
 function solution(numbers) {
-    const ans = new Set();
+    let ans = new Set(); // 중복 제거를 위한 Set 자료구조
     
-    const isPrime = (num) => {
+    const isPrime = (num) => { // 소수 판별 함수
         if(num < 2) return false;
         for(let i = 2; i <= Math.sqrt(num); i++){
             if(num % i === 0) return false;
         }
+        
         return true;
     }
     
-    const DFS = (numArr, fixNum) => {
-        if(numArr.length === 0) return;
+    const DFS = (fixNum, array) => {
+        if(array.length === 0) return; // 더 이상 배열의 요소가 없다면 리턴
         
-        for(let i = 0; i < numArr.length; i++){
-            if(isPrime(parseInt(fixNum + numArr[i]))) {
-                ans.add(parseInt(fixNum + numArr[i]));
+        for(let i = 0; i < array.length; i++){
+            // 고정된 숫자와 다음 숫자를 더한 값이 소수일 때
+            if(isPrime(parseInt(fixNum + array[i]))){
+                ans.add(parseInt(fixNum + array[i]));
             }
-            
-            const temp = [...numArr];
-            temp.splice(i, 1);
-            DFS(temp, fixNum + numArr[i]);
+            const temp = [...array];
+            temp.splice(i, 1); // 위에서 더한 다음 숫자를 제외한 배열
+            DFS(fixNum + array[i], temp); // 고정된 숫자에 다음 숫자 더하기, 다음 숫자를 제외한 배열
         }
-    };
+    }
     
-    DFS(numbers.split(""), "");
+    DFS("", numbers.split(""));
     
-    return ans.size;
+    return ans.size; // 중복이 제거된 Set의 길이
 }
