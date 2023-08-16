068d8812d027dbe38308fcdb09a63b1640614c50
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-16 21:51:50 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 83.14 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,19 +1,14 @@
 function solution(elements) {
-    let ans = new Set(); // 중복 제거용 Set 자료구조
+    const ans = new Set(); // 중복 제거를 위한 Set
+    const temp = elements.concat(elements); // 원형 수열로 사용할 배열 생성
     
-    for(let i = 1; i <= elements.length; i++) { // 순열의 길이
-        let sum = 0; // 합
-        for(let j = 0; j < elements.length; j++) { // 순열 순회
-            if(j === 0) { // 순열의 첫번째일 때
-                for(let k = 0; k < i; k++) { // 초기 합 구하기
-                    sum += elements[k];
-                }
-            } else {
-                sum -= elements[j - 1]; // 이전 값 빼기
-                sum += elements[(j + i - 1) % elements.length]; // 다음 값 더하기
-            }
+    for(let i = 0; i < elements.length; i++){ // 부분 수열의 길이
+        let sum = 0;
+        for(let j = 0; j < elements.length; j++){ // 수열 순회
+            sum += temp[i + j];
             ans.add(sum);
         }
     }
+    
     return ans.size;
 }
