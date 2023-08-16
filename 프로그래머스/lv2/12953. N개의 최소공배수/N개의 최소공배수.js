245190c47c2280369489f2daa1b3ffe2a5a4d377
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-16 20:29:45 +0900

    [level 2] Title: N개의 최소공배수, Time: 7.35 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,17 +1,17 @@
 function solution(arr) {
-  let ans = 0;
-  let maxNum = Math.max(...arr);
-  let count = 0;
-
-  while (count < arr.length) {
-    count = 0;
-    ans += maxNum;
-    for (let i = 0; i < arr.length; i++) {
-      if (ans % arr[i] === 0) {
-        count += 1;
-      }
+    let ans = 0;
+    const max = Math.max(...arr); // 배열 중 가장 큰 값
+    let count = 0; // 모든 요소가 공배수인지 확인하는 변수
+    
+    while(count < arr.length){ // 모든 요소가 공배수면 종료
+        count = 0;
+        ans += max;
+        for(const item of arr){
+            if(ans % item === 0){ // ans가 공배수라면
+                count += 1;
+            }
+        }
     }
-  }
-
-  return ans;
+    
+    return ans;
 }
