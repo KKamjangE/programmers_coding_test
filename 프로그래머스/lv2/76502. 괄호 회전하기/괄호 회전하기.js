be255c32a21949b9652797b8cc839ca650f7b451
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-16 22:25:25 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,34 +1,32 @@
 function solution(s) {
-  if (s.length % 2 === 1) return 0;
-  let ans = 0;
-
-  const check = (str) => {
-    const stack = [];
-
-    for (let i = 0; i < str.length; i++) {
-      if (str[i] === "[") {
-        stack.push(str[i]);
-      } else if (str[i] === "(") {
-        stack.push(str[i]);
-      } else if (str[i] === "{") {
-        stack.push(str[i]);
-      } else if (str[i] === ")" && stack[stack.length - 1] === "(") {
-        stack.pop();
-      } else if (str[i] === "]" && stack[stack.length - 1] === "[") {
-        stack.pop();
-      } else if (str[i] === "}" && stack[stack.length - 1] === "{") {
-        stack.pop();
-      } else {
-        return 0;
-      }
+    let ans = 0;
+    
+    const check = (arr) => { // 올바른 괄호 문자열인지 확인하는 함수
+        const stack = [];
+        for(const item of arr) {
+            if(item === "[" || item === "(" || item === "{"){
+                stack.push(item);
+            }
+            if(item === ")"){
+                if(stack[stack.length - 1] === "(") stack.pop();
+                else return 0
+            }
+            if(item === "]"){
+                if(stack[stack.length - 1] === "[") stack.pop();
+                else return 0
+            }
+            if(item === "}"){
+                if(stack[stack.length - 1] === "{") stack.pop();
+                else return 0
+            }
+        }
+        return stack.length === 0 ? 1 : 0;
     }
-    return stack.length > 0 ? 0 : 1;
-  };
-
-  for (let i = 0; i < s.length; i++) {
-    ans += check(s);
-    s = s.substr(1) + s.substr(0, 1);
-  }
-
-  return ans;
+    
+    for(let i = 0; i < s.length; i++){ // 문자열 길이만큼 반복
+        ans += check(s); // 올바른 괄호 문자열이라면 + 1
+        s = s.substring(1) + s.substring(0, 1); // 맨 앞의 문자를 맨 뒤로
+    }
+    
+    return ans;
 }
