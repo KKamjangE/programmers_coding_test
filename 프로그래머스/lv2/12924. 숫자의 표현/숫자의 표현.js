53009f082e92246c54a274247f7a616f08b94b2c
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-16 15:09:33 +0900

    [level 2] Title: 숫자의 표현, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,11 +1,11 @@
 function solution(n) {
-  let ans = 0;
-
-  for (let i = 0; i <= n; i++) {
-    if (n % i === 0 && i % 2 === 1) {
-      ans++;
+    let ans = 0;
+    
+    for(let i = 0; i <= n; i++){
+        if(n % i === 0 && i % 2 === 1){ // 소수일 경우
+            ans += 1;
+        }
     }
-  }
-
-  return ans;
+    
+    return ans;
 }
