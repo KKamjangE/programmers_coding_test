c5fc79a3a837ac92aa9bf0c01fa280ca62496b50
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-16 20:45:14 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,12 +1,10 @@
 function solution(n) {
-  let ans = 0;
-  const arr = new Array(n + 1).fill(0);
-  arr[0] = 1;
-  arr[1] = 1;
-
-  for (let i = 2; i < arr.length; i++) {
-    arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567;
-  }
-
-  return arr[arr.length - 1];
+    const F = new Array(n + 1).fill(0);
+    F[0] = 1;
+    F[1] = 1;
+    for(let i = 2; i <= n; i++){
+        F[i] = (F[i - 1] + F[i - 2]) % 1234567;
+    }
+    
+    return F[F.length - 1];
 }
