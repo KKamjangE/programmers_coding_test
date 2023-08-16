d5c62fb83911250b063fa6e3fb4a96449d75ca98
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-16 17:46:37 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,14 +1,14 @@
 function solution(n) {
-  let ans = 0;
-
-  while (n != 0) {
-    if (n % 2 != 0) {
-      ans += 1;
-      n -= 1;
-    } else {
-      n /= 2;
+    let ans = 0;
+    
+    while(n !== 0) {
+        if(n % 2 === 0){
+            n /= 2;   
+        } else {
+            n -= 1;
+            ans += 1;
+        }
     }
-  }
-
-  return ans;
+    
+    return ans;
 }
