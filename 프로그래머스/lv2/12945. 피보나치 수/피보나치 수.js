aabc5386247cd9b40e3caef44d551ee5677363cf
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-16 15:39:55 +0900

    [level 2] Title: 피보나치 수, Time: 3.20 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -1,9 +1,10 @@
 function solution(n) {
-  let arr = new Array(n + 1).fill(0);
-  arr[1] = 1;
-
-  for (let i = 2; i < arr.length; i++) {
-    arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567;
-  }
-  return arr[arr.length - 1];
+    const F = new Array(n + 1).fill(0); // 0으로 채운 배열
+    F[1] = 1; // 1번째 요소는 1
+    
+    for(let i = 2; i < F.length; i++){ // i - 1과 i - 2를 더해가며 i번째 요소 채우기
+        F[i] = (F[i - 1] + F[i - 2]) % 1234567;
+    }
+    
+    return F[F.length - 1];
 }
