cdd20a802d2c85f73dddfed71f598a557f00f28e
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-16 21:01:26 +0900

    [unrated] Title: 귤 고르기, Time: 69.45 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
@@ -1,19 +1,21 @@
-function solution(k, tangerine) {
-  let ans = 0;
-  let obj = {};
-  tangerine.forEach((value) => {
-    obj[value] = ++obj[value] || 1;
-  });
-
-  const objValueToArr = Object.values(obj).sort((a, b) => b - a);
-  let sum = 0;
-
-  objValueToArr.forEach((value) => {
-    if (sum < k) {
-      ans += 1;
-      sum += value;
+function solution(k, tangerine){
+    let ans = 0;
+    const obj = {};
+    
+    for(const item of tangerine){ // 같은 크기의 귤 갯수 카운팅
+        obj[item] = (obj[item] || 0) + 1;
     }
-  });
-
-  return ans;
+    
+    const temp = Object.values(obj).sort((a, b) => b - a); // 객체의 값을 내림차순 정렬
+    
+    let oranges = 0; // 상자에 담은 귤 갯수
+    for(const item of temp){
+        ans += 1; // 귤의 크기의 종류
+        oranges += item;
+        if(oranges >= k){
+            break;
+        }
+    }
+    
+    return ans;
 }
