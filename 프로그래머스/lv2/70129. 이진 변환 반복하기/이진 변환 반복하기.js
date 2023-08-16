766916688ba6bd874f6bf621a0f21f743c0e1a60
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-16 12:54:10 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 7.79 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,24 +1,21 @@
 function solution(s) {
-  let zeroCount = 0;
-  let trasCount = 0;
-  while (true) {
-    if (s === "1") {
-      break;
+    let count = 0; // 변환 횟수
+    let zero = 0; // 제거한 0의 개수
+    
+    while(true) {
+        if(s === "1") break; // "1"이 되면 종료
+        
+        const temp = [...s].map((item) => {
+            if(item === "1") { // "1"이면 그대로 반환
+                return "1"
+            } else { // "0"이면 제거
+                zero += 1;
+            }
+        }).join("");
+        
+        s = temp.length.toString(2); // temp의 길이를 2진수로 변환
+        count += 1; // 변환 횟수 + 1
     }
-
-    s = s
-      .split("")
-      .map((value) => {
-        if (value === "0") {
-          zeroCount += 1;
-        } else {
-          return value;
-        }
-      })
-      .join("");
-    s = s.length.toString(2);
-    trasCount += 1;
-  }
-
-  return [trasCount, zeroCount];
+    
+    return [count, zero];
 }
