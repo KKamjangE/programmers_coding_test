76a62abaeb6940735d963a4de6f51aadfe7966c6
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-17 15:59:06 +0900

    [level 2] Title: [1차] 캐시, Time: 0.55 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,24 +1,24 @@
 function solution(cacheSize, cities) {
-  let ans = 0;
-  let cache = [];
-  if (cacheSize === 0) return cities.length * 5;
-  cities = cities.map((value) => value.toLowerCase());
-
-  cities.forEach((value) => {
-    if (cache.indexOf(value) === -1) {
-      if (cache.length < cacheSize) {
-        cache.push(value);
-      } else if (cache.length === cacheSize) {
-        cache.shift();
-        cache.push(value);
-      }
-      ans += 5;
-    } else {
-      cache.splice(cache.indexOf(value), 1);
-      cache.push(value);
-      ans += 1;
+    if(cacheSize === 0) return cities.length * 5;
+    let ans = 0;
+    const cache = [];
+    cities = cities.map((item) => item.toLowerCase());
+    
+    for(const item of cities){
+        if(cache.indexOf(item) === -1){ // 캐시에 동일한 데이터가 없다면
+            if(cache.length < cacheSize){ // 캐시 공간이 남아있다면
+                cache.push(item); // 캐시에 저장
+            } else {
+                cache.shift(); // 캐시에서 오래된 데이터 제거
+                cache.push(item); // 캐시에 저장
+            }
+            ans += 5;
+        } else { // 캐시에 동일한 데이터가 있다면
+            cache.splice(cache.indexOf(item), 1); // 해당 데이터 제거
+            cache.push(item); // 캐시에 저장
+            ans += 1;
+        }
     }
-  });
-
-  return ans;
+    
+    return ans;
 }
