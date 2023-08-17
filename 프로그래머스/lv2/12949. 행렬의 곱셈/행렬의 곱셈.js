62fb1a751d10cf0902428995b256fc50ba530b8b
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-08-17 14:28:59 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.94 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -2,7 +2,7 @@ function solution(arr1, arr2) {
     const ans = [];
     
     for(let i = 0; i < arr1.length; i++){
-        let result = [];
+        const result = [];
         for(let j = 0; j < arr2[0].length; j++){
             let temp = 0;
             for(let k = 0; k < arr2.length; k++){
