b2916f8a1aa1b8843df1f80985ca7d45ed33eb48
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-09-20 09:11:19 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,9 +1,6 @@
 function solution(s) {
-    let ans = 0;
     const arr = s.split(" ");
-    
     const max = Math.max(...arr);
     const min = Math.min(...arr);
-    
     return `${min} ${max}`;
 }
