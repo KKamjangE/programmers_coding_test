bb530fe6e6c924414b6d953d9bcb87c1cd5fb77a
Jemin <105330204+KKamjangE@users.noreply.github.com>
2023-09-22 09:32:18 +0900

    [level 2] Title: 올바른 괄호, Time: 5.31 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,20 +1,17 @@
 function solution(s){
-    if(s.length % 2 !== 0) return false;
     
-    const stack = [];
+    let stack = 0;
     
     for(const item of s){
-        if(item === "(") { // 열린 괄호는 스택에 저장
-            stack.push(item);
-        }
-        if(item === ")"){ // 닫힌 괄호일 때
-            if(stack.length > 0){ // 스택에 요소가 있다면 pop
-                stack.pop();
-            } else { // 스택에 아무것도 없다면 올바르지 않은 괄호
-                return false;
-            }
+        if(item === "("){
+            stack += 1;
+        } else if(stack > 0 && item === ")"){
+            stack -= 1;
+        } else {
+            return false;
         }
     }
     
-    return stack.length > 0 ? false : true; // 스택에 아무것도 남아있지 않다면 올바른 괄호
+    if(stack === 0) return true;
+    else return false;
 }
