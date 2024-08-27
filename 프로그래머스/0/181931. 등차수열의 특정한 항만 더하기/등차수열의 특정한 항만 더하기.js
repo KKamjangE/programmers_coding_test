4af948a5786f40b46cd1f977724d287951c2b6f4
Jemin <105330204+KKamjangE@users.noreply.github.com>
2024-08-27 09:57:18 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -1,5 +1,3 @@
 function solution(a, d, included) {
-    let sum = 0;
-    included.forEach((item, index) => sum += item ? a + (index * d) : 0)
-    return sum
+    return included.reduce((acc, curr, index) => curr ? acc + a + (index * d) : acc ,0)
 }
