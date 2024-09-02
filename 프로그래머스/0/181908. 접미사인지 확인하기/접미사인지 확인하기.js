d3cf786d5fe0db6f89a9096f43a53d084cf12e93
Jemin <105330204+KKamjangE@users.noreply.github.com>
2024-09-02 10:25:06 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,3 +1,3 @@
 function solution(my_string, is_suffix) {
-    return +Boolean(my_string.split("").map((_, index) => my_string.slice(index)).find((item) => item === is_suffix))
+    return +my_string.endsWith(is_suffix)
 }
