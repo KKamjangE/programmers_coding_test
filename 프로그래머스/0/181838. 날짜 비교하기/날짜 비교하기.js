38d572c9621652710611f136eef3ecbcf7e3fc31
Jemin <105330204+KKamjangE@users.noreply.github.com>
2024-10-02 15:46:03 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.18 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,6 +1,3 @@
 function solution(date1, date2) {
-    const convertArrToDate = (date) => {
-        return new Date(date[0], date[1], date[2])
-    }
-    return +(convertArrToDate(date1) < convertArrToDate(date2))
+    return +(new Date(date1) < new Date(date2))
 }
