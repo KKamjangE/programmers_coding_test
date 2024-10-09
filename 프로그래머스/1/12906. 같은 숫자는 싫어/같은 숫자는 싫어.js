21701af05ca7b1960f5033f34892d0f54a7f92c5
Jemin <105330204+KKamjangE@users.noreply.github.com>
2024-10-09 19:23:52 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 51.03 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,10 +1,4 @@
-function solution(arr) {
-    return arr.reduce((acc, curr) => {
-        if(acc.length === 0) {
-            acc.push(curr)
-            return acc
-        }
-        if(acc[acc.length-1] != curr) acc.push(curr)
-        return acc
-    }, [])
+function solution(arr)
+{
+    return arr.filter((item, index) => arr[index - 1] !== item)
 }
