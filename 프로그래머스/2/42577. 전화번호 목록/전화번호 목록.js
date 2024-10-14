6374b2027595623bf5b3085bbde402bde48d2253
Jemin <105330204+KKamjangE@users.noreply.github.com>
2024-10-14 09:41:33 +0900

    [level 2] Title: 전화번호 목록, Time: 157.54 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,6 +1,6 @@
 function solution(phone_book) {
     return !phone_book.sort().some((item, index) => {
-        if(index === phone_book.length - 1) return false
-        return phone_book[index + 1].startsWith(phone_book[index])
+        if(!phone_book[index + 1]) return false
+        return phone_book[index + 1].startsWith(item)
     })
 }
