fe4941a60045dd5394015823698e67b7ad88dd70
Jemin <105330204+KKamjangE@users.noreply.github.com>
2024-12-10 10:01:35 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 36.72 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -2,9 +2,8 @@ function solution(maps) {
     const my = maps.length
     const mx = maps[0].length
     const visited = Array.from({length: my}, () => Array.from({length: mx}, () => -1))
+    const dy = [1, -1, 0, 0]
     const dx = [0, 0, 1, -1]
-    const dy = [1, -1 ,0 ,0]
-
     const que = [[0, 0]]
     visited[0][0] = 1
     
@@ -12,14 +11,13 @@ function solution(maps) {
         const [y, x] = que.shift()
         
         for(let i=0;i<4;i++){
-            const nx = x + dx[i]
             const ny = y + dy[i]
-            if(0 <= nx && nx < mx && 0 <=ny && ny < my && visited[ny][nx] === -1 && maps[ny][nx] === 1) {
+            const nx = x + dx[i]
+            if(0 <= nx && nx < mx && 0 <= ny && ny < my && maps[ny][nx] !== 0 && visited[ny][nx] === -1){
                 que.push([ny, nx])
                 visited[ny][nx] = visited[y][x] + 1
             }
         }
     }
-    
     return visited[my - 1][mx - 1]
 }
